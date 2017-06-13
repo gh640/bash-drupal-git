be6bca37d4fd37e12b8569120ddd9aa23fd57905
Goto Hayato <habita.gh@gmail.com>
2017-06-13 22:24:10 +0900

    Add dgit function and module list for auto-completion.

---
+++ init_dgit.sh
@@ -0,0 +1,23 @@
+function dgit {
+  if [ -z "$1" ]; then
+    echo 'usage: dgit [URL]'
+    return
+  fi
+
+  git clone "https://git.drupal.org/project/${1}.git"
+}
+
+_dgit_completion()
+{
+  local cur prev cword opts
+  local directory
+
+  _get_comp_words_by_ref -n : cur prev cword
+
+  directory=$(dirname "${BASH_SOURCE[0]}")
+  opts=$(cat $directory/drupal_modules.csv)
+
+  COMPREPLY=($(compgen -W "${opts}" -- "${cur}"))
+}
+
+complete -F _dgit_completion dgit
