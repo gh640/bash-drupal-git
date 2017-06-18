dff076570458050af2f5a7143a7d9268b3c3ad86
Goto Hayato <habita.gh@gmail.com>
2017-06-18 15:42:21 +0900

    Add dpj command. Add comments in the script file.

--- init_dgit.sh
+++ init_dgit.sh
@@ -1,3 +1,6 @@
+#!/bin/bash
+
+# Fetches a drupal project with `git clone`.
 dgit() {
   if [ -z "$1" ]; then
     echo "usage: ${FUNCNAME[0]} [URL]"
@@ -7,6 +10,36 @@ dgit() {
   git clone "https://git.drupal.org/project/${1}.git"
 }
 
+# Opens pages for drupal projects.
+dpj() {
+  if [ -z "$1" ]; then
+    echo "usage: ${FUNCNAME[0]} [URL]"
+    return
+  fi
+
+  local arg is_queue args_stripped
+
+  args_stripped=""
+  is_queue=false
+  for arg in "$@"; do
+    if [ "$arg" == "-q" ]; then
+      is_queue=true
+    else
+      args_stripped=${args_stripped:+$args_stripped }$arg
+    fi
+  done
+
+  echo "$args_stripped"
+  for arg in $args_stripped; do
+    if $is_queue; then
+      open "https://www.drupal.org/project/issues/${arg}?categories=All"
+    else
+      open "https://www.drupal.org/project/${arg}"
+    fi
+  done
+}
+
+# Provides auto-completion for dgit.
 _dgit_completion() {
   local cur prev cword opts
   local directory
@@ -19,4 +52,4 @@ _dgit_completion() {
   COMPREPLY=($(compgen -W "${opts}" -- "${cur}"))
 }
 
-complete -F _dgit_completion dgit
+complete -F _dgit_completion dgit dpj
